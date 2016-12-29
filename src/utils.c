c4f26179f732725849529daa3124aed27d019106
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2016-12-29 14:29:08 -0500

    added more functions to read larger integers

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,7 @@
 #include "utils.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 unsigned read_uint(void *_buffer, unsigned offset, unsigned size)
 {
@@ -8,7 +10,37 @@ unsigned read_uint(void *_buffer, unsigned offset, unsigned size)
     unsigned char *buffer = _buffer;
     if (sizeof(result) < size)
     {
-        fprintf(stderr, "ERROR: requested size (%d) larger than sizeof(unsigned) = %lu.\n", size, sizeof(result));
+        fprintf(stderr, "ERROR: requested size (%d) larger than sizeof(unsigned int) = %lu.\n", size, sizeof(result));
+    }
+
+    for (i = offset; i < offset + size; i++)
+        result = (result << 8) | buffer[i];
+    return result;
+}
+
+unsigned long read_ulong(void *_buffer, unsigned offset, unsigned size)
+{
+    unsigned i = 0;
+    unsigned long result = 0;
+    unsigned char *buffer = _buffer;
+    if (sizeof(result) < size)
+    {
+        fprintf(stderr, "ERROR: requested size (%d) larger than sizeof(unsigned long) = %lu.\n", size, sizeof(result));
+    }
+
+    for (i = offset; i < offset + size; i++)
+        result = (result << 8) | buffer[i];
+    return result;
+}
+
+unsigned long long read_ulonglong(void *_buffer, unsigned offset, unsigned size)
+{
+    unsigned i = 0;
+    unsigned long long result = 0;
+    unsigned char *buffer = _buffer;
+    if (sizeof(result) < size)
+    {
+        fprintf(stderr, "ERROR: requested size (%d) larger than sizeof(unsigned long long) = %lu.\n", size, sizeof(result));
     }
 
     for (i = offset; i < offset + size; i++)
@@ -23,10 +55,20 @@ unsigned read_int(void *_buffer, unsigned offset, unsigned size)
     unsigned char *buffer = _buffer;
     if (sizeof(result) < size)
     {
-        fprintf(stderr, "ERROR: requested size (%d) larger than sizeof(unsigned) = %lu.\n", size, sizeof(result));
+        fprintf(stderr, "ERROR: requested size (%d) larger than sizeof(int) = %lu.\n", size, sizeof(result));
     }
 
     for (i = offset; i < offset + size; i++)
         result = (result << 8) | buffer[i];
+    return result;
+}
+
+const char *epoch_to_string(time_t in_time)
+{
+    char *result = (char *)malloc(sizeof(char)*100);
+    struct tm ts;
+    ts = *localtime(&in_time);
+    strftime(result, sizeof(char) * 100, "%a %Y-%m-%d %H:%M:%S %Z", &ts);
+
     return result;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,12 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
+#include <time.h>
+
 unsigned read_uint(void *buffer, unsigned offset, unsigned size);
+unsigned long read_ulong(void *buffer, unsigned offset, unsigned size);
+unsigned long long read_ulonglong(void *buffer, unsigned offset, unsigned size);
 unsigned read_int(void *buffer, unsigned offset, unsigned size);
+const char *epoch_to_string(time_t time);
 
 #endif /* __UTILS_H */
